de023cfb436f918e32e0703dedf91d8f7f28fb7f
talk2suhal-ctrl <talk2suhal@gmail.com>
2026-02-28 17:51:48 +0100

    Update gene_expression (3).py

--- gene_expression (3).py
+++ gene_expression (3).py
@@ -33,8 +33,7 @@ if uploaded_file is not None:
     st.write("---")
     st.write("### Analyse FASTA Sequence")
 
-    fasta_input = st.text_area("Enter a FASTA sequence", height=200)
-if fasta_input:
+if fasta_input = st.text_area("Enter a FASTA sequence", height=200)
   fasta_io = StringIO(fasta_input)
   sequences = list(SeqIO.parse(fasta_io, "fasta"))
   for record in SeqIO.parse(fasta_io, "fasta"):
